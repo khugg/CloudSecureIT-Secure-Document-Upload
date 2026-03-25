406a5d6f9197fc019a4d1f6b3b7152caee73cd20
Viviane Huguette <kemshuggs@gmail.com>
2026-03-25 19:44:45 -0300

    Add submissions tracking and submissions page

--- src/app.js
+++ src/app.js
@@ -5,9 +5,15 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
+
+
+
+
 
 
 
@@ -15,6 +21,18 @@ app.use(express.static(path.join(__dirname, "../public")));
 if (!fs.existsSync("uploads")) {
   fs.mkdirSync("uploads");
 }
+
+// create data if they don't exist
+if (!fs.existsSync("data")) {
+  fs.mkdirSync("data");
+}
+
+const submissionsFilePath = path.join(__dirname, "../data/submissions.json");
+
+if (!fs.existsSync(submissionsFilePath)) {
+  fs.writeFileSync(submissionsFilePath, "[]");
+}
+
 
 //  storage configuration
 const storage = multer.diskStorage({
@@ -45,6 +63,31 @@ app.post("/upload", upload.single("file"), (req, res) => {
       return res.status(400).json({ message: "All fields are required" });
     }
 
+
+
+const fileUrl = `http://localhost:3000/uploads/${file.filename}`;
+
+    const newSubmission = {
+      id: Date.now(),
+      firstName,
+      lastName,
+      email,
+      documentType,
+      originalFileName: file.originalname,
+      storedFileName: file.filename,
+      fileUrl,
+      submittedAt: new Date().toISOString()
+    };
+
+    const existingData = fs.readFileSync(submissionsFilePath, "utf-8");
+    const submissions = JSON.parse(existingData);
+
+    submissions.push(newSubmission);
+
+    fs.writeFileSync(submissionsFilePath, JSON.stringify(submissions, null, 2));
+
+
+
     res.json({
       message: "Thank you for your submission. We will contact you shortly.",
       user: {
@@ -54,7 +97,7 @@ app.post("/upload", upload.single("file"), (req, res) => {
         documentType
       },
       fileName: file.filename,
-      fileUrl: `http://localhost:3000/uploads/${file.filename}`
+      fileUrl,
     });
   } catch (error) {
     console.error("Upload error :", error);
@@ -64,13 +107,38 @@ app.post("/upload", upload.single("file"), (req, res) => {
 
 
 
+// submissions path
+
+app.get("/submissions", (req, res) => {
+  try {
+    if (!fs.existsSync(submissionsFilePath)) {
+      return res.json([]);
+    }
+
+    const data = fs.readFileSync(submissionsFilePath, "utf-8");
+
+    if (!data.trim()) {
+      return res.json([]);
+    }
+
+    const submissions = JSON.parse(data);
+    res.json(submissions);
+  } catch (error) {
+    console.error("Error reading submissions:", error);
+    res.status(500).json({ message: "Error reading submissions" });
+  }
+});
+
+
 
 
 // make accessible
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 const PORT = 3000;
+   
 
+// start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
